3168cd31890d2840cdcd615c74183d84222cd8f8
noname44k1 <37573795+noname44k1@users.noreply.github.com>
2024-11-12 16:06:16 +0700

    Update chatbot_streamlit.py

--- chatbot_streamlit.py
+++ chatbot_streamlit.py
@@ -22,8 +22,7 @@ if prompt := st.chat_input():
     #     st.info("Please add your OpenAI API key to continue.")
     #     st.stop()
 
-    # client = OpenAI(api_key=openai_api_key)
-    client = openai.OpenAI(api_key="sk-w6Nr2w5AJEw6owm4C3Bc0dEf763a40C9B4965981C227FbA7",base_url="https://yescale.one/v1")
+    client = openai.OpenAI(api_key=os.getenv('OPENAI_API_KEY'),base_url=os.getenv('OPENAI_API_BASE'))
     st.session_state.messages.append({"role": "user", "content": prompt})
     st.chat_message("user").write(prompt)
     response = client.chat.completions.create(model="gpt-3.5-turbo", messages=st.session_state.messages)
