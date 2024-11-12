c56f0ddfcae6b8548a3d1297b533972d501d9bb5
noname44k1 <37573795+noname44k1@users.noreply.github.com>
2024-11-12 17:18:22 +0700

    Update chatbot_streamlit.py

--- chatbot_streamlit.py
+++ chatbot_streamlit.py
@@ -5,9 +5,9 @@ import os
 
 with st.sidebar:
     #openai_api_key = st.text_input("OpenAI API Key", key="chatbot_api_key", type="password")
-    "[Get an OpenAI API key](https://platform.openai.com/account/api-keys)"
-    "[View the source code](https://github.com/streamlit/llm-examples/blob/main/Chatbot.py)"
-    "[![Open in GitHub Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/streamlit/llm-examples?quickstart=1)"
+    #"[Get an OpenAI API key](https://platform.openai.com/account/api-keys)"
+    #"[View the source code](https://github.com/streamlit/llm-examples/blob/main/Chatbot.py)"
+    #"[![Open in GitHub Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/streamlit/llm-examples?quickstart=1)"
 
 st.title("💬 Chatbot - Vinh City - Nghe An Province")
 st.caption("🚀 A Streamlit chatbot powered by OpenAI")
